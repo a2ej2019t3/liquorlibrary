07d2983eac11277042010441e3ca0f4eff230056
JunboJ <junboz598@gmail.com>
2019-05-08 15:33:49 +1200

    create foot and head; migrate to bs4

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+  <?php
+    include_once ("partials/head.php");
+  ?>
 </head>
 <body>
     <?php
-    include ("partials/header.php");
+    include_once ("partials/header.php");
     ?>
     <section>
-      
       <div class="container-fluid">
             <div class="row">
               <div class="col-sm-12 col-md-12 col-lg-12" id="mainscreen"></div>
             </div>
-      </div>
+        </div>
     </section>
     <br><br><br><br>
     <section id="aboutus">
@@ -26,21 +27,13 @@
                 <img class="brandlogo" src="images/brandlogo.jpg" alt="">
               </div>
             </div>
-      </div>
+        </div>
 
     </section>
-    <!-- bootstrap /js -->
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> -->
-
-    <script type="text/javascript" src="main.js"></script>
-    <script type="text/javascript" src="search.js"></script>
-s
- 
+  <?php
+    include_once ("partials/foot.php");
+  ?>
 
 </body>
 </html>
