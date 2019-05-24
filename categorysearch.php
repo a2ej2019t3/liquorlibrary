d9a312f2d13a0ba59b374ab6f9e2d5eee3ebc7cb
JunboJ <junboz598@gmail.com>
2019-05-24 15:51:13 +1200

    front end cart

--- categorysearch.php
+++ categorysearch.php
@@ -2,7 +2,7 @@
     session_start();
     $_SESSION['location'] = 'productlist';
     if (isset($_SESSION['user'])) {
-      $user = $_SESSION['user'];
+        $user = $_SESSION['user'];
     }
     include ('connection.php');
     // category search 
@@ -21,56 +21,49 @@
         alert("result empty");
     }
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php
-    include_once ("partials/head.php");
-  ?>
-
+    <?php
+        include_once ("partials/head.php");
+    ?>
 <title>Product_listbyCategory</title>
- </head>
- <body style="height: 110%;">
+</head>
+<body style="height: 110%;">
     <section>
         <?php
-          include_once ("partials/header.php");
+            include_once ("partials/header.php");
         ?>        
     </section>
     <br><br>
-    <span id="debug"></span>
-<?php
-include ("Cart/stickycart.php");
-?>
-<div class="container_fluid">
-    <div class="row">
-<!-- content body starts -->
-        <div class="sidenavbar col-md-3 col-xs-12 content-left" style="text-align:center;">
-            <!-- sideNave -->
-                <?php
-                    include_once ("partials/sideNav.php");
-                ?>
+    <section id="main">
+        <?php
+            include ("Cart/stickycart.php");
+        ?>
+        <div class="container_fluid">
+            <div class="row">
+                <!-- content body starts -->
+                <div class="sidenavbar col-md-3 col-xs-12 content-left" style="text-align:center;">
+                    <!-- sideNave -->
+                    <?php
+                        include_once ("partials/sideNav.php");
+                    ?>
+                </div>
+
+                <div class="productresult col-md-9 col-xs-12 content-right">
+                    <!-- product list results -->
+                    <article id="content">
+                        <?php
+                        include_once ("typesearch.php");
+                        ?>
+                    </article>
+                </div>
+            </div>
         </div>
-
-        <div class="productresult col-md-9 col-xs-12 content-right">
-            <!-- product list results -->
-              <article id="content">
-                <?php
-                include_once ("typesearch.php");
-                
-
-                ?>
-                </article>
-        </div>
-
-    </div>
-
-</div>
-
- <?php
-    include_once ("partials/foot.php");
-  ?>  
+    </section>
+      <?php
+          include_once ("partials/foot.php");
+      ?>  
   <script type="text/javascript" src="js/sub.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
   <script type="text/javascript" src="js/search.js"></script>
