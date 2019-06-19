f9a9436ddb23efbcbd29cce51558253df73026c1
ham8821 <ham38538821@gmail.com>
2019-06-19 13:48:52 +1200

    chart_added

--- branchreport.php
+++ branchreport.php
@@ -266,6 +266,7 @@ chart.render();
 </div>
 <br>
 <div class="row">
+  <!-- Line chart -->
 <div class="col-xl-8 col-lg-7">
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
@@ -282,7 +283,23 @@ chart.render();
               </div>
             </div>
 </div>
+<!-- pie chart -->
 
+  <div class="col-xl-4 col-lg-5">
+                <div class="card shadow mb-4">
+                  <!-- Card Header - Dropdown -->
+                  <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Pickup Orders :Earnings Overview</h6>
+                  </div>
+                  <!-- Card Body -->
+                  <div class="card-body">
+                    <div class="chart-area">
+                    <div id="chartContainer" style="height: 370px; width: 100%;"></div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
 </div>
 
 <!-- ---------------------------------------------------------------------------------------------------------- -->
--- partials/branchearningchart.php
+++ partials/branchearningchart.php
@@ -17,55 +17,62 @@ $get_day = array('1' =>"Jan",
 if($selectedmonth !=null){
     // $get_startmonth= $selectedmonth-11;
     $montharray = array();
+    $yeararray = array();
     $incomearray = array();
     
     $b=0;
+    $e=0;
     for($a=11;$a>=0;$a--){
         $get_startmonth= $selectedmonth-$a;
         if($get_startmonth<=0){
             $pointmonth=$get_startmonth+12;
-           
-
+            $yeararray[$b]=$selectedyear-1;
+            
         }
         else{
             $pointmonth=$get_startmonth;
+            $yeararray[$b]=$selectedyear;
         }
             $montharray[$b]=$pointmonth;
-                $b++;
-                
+            $b++;
+
     }
+    // var_dump($yeararray);
 //   echo $montharray[0];
     if(!empty($montharray)){
-        $totalpickup_earning=0;
+        
         for($c=0;$c<count($montharray);$c++){
-            $earning_query="SELECT * from orders where whID='$whID' AND deliverymethod='pickup' AND status=4  AND extract(month from date) = '$montharray[$c]'";
+            $earning_query="SELECT * from orders where whID='$whID' AND deliverymethod='pickup' AND status=4  AND extract(month from date) = $montharray[$c] AND extract(year from date) = $yeararray[$c]";
             $earning_res=mysqli_query($connection, $earning_query);
-            // echo $montharray[$c];
+            // echo $montharray[12];
             if ( $earning_res !=""){
                 $earning_arr=mysqli_fetch_all($earning_res);
-                $sum=$earning_arr[$c][6];
-                echo $sum;
+                $totalpickup_earning=0;
+                for($d=0;$d<count($earning_arr);$d++){
+                $sum=$earning_arr[$d][6];   
                 $totalpickup_earning=$totalpickup_earning+$sum;
-
-                echo $totalpickup_earning;
+                }
+                // monthly total earning
+                //  echo $totalpickup_earning;
+                 $incomearray[$c]=$totalpickup_earning;
             }
         }
     }
-    // print_r( $montharray);
+    // print_r( $incomearray);
   
         $dataPoints = array(
-            array("y" => 25, "label" => $get_day[$montharray[0]]),
-            array("y" => 15, "label" => $get_day[$montharray[1]]),
-            array("y" => 25, "label" => $get_day[$montharray[2]]),
-            array("y" => 5, "label" => $get_day[$montharray[3]]),
-            array("y" => 5, "label" => $get_day[$montharray[4]]),
-            array("y" => 5, "label" => $get_day[$montharray[5]]),
-            array("y" => 25, "label" => $get_day[$montharray[6]]),
-            array("y" => 35, "label" => $get_day[$montharray[7]]),
-            array("y" => 5, "label" => $get_day[$montharray[8]]),
-            array("y" => 10, "label" =>  $get_day[$montharray[9]]),
-            array("y" => 0, "label" =>  $get_day[$montharray[10]]),
-            array("y" => 20, "label" =>  $get_day[$montharray[11]])
+            array("y" => $incomearray[0], "label" => $yeararray[0].$get_day[$montharray[0]]),
+            array("y" => $incomearray[1], "label" => $yeararray[1].$get_day[$montharray[1]]),
+            array("y" => $incomearray[2], "label" => $yeararray[2].$get_day[$montharray[2]]),
+            array("y" => $incomearray[3], "label" => $yeararray[3].$get_day[$montharray[3]]),
+            array("y" => $incomearray[4], "label" => $yeararray[4].$get_day[$montharray[4]]),
+            array("y" => $incomearray[5], "label" => $yeararray[5].$get_day[$montharray[5]]),
+            array("y" => $incomearray[6], "label" => $yeararray[6].$get_day[$montharray[6]]),
+            array("y" => $incomearray[7], "label" => $yeararray[7].$get_day[$montharray[7]]),
+            array("y" => $incomearray[8], "label" => $yeararray[8].$get_day[$montharray[8]]),
+            array("y" => $incomearray[9], "label" =>  $yeararray[9].$get_day[$montharray[9]]),
+            array("y" => $incomearray[10], "label" =>  $yeararray[10].$get_day[$montharray[10]]),
+            array("y" => $incomearray[11], "label" =>  $yeararray[11].$get_day[$montharray[11]])
         );
    
 }
