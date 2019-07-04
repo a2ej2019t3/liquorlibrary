dab5eb0872c0627d8cf1cc428695fab6245e1c3b
JunboJ <junboz598@gmail.com>
2019-07-04 16:45:15 +1200

    update connection for AWS deploy

--- database/DBconn.php
+++ database/DBconn.php
@@ -1,27 +1,29 @@
 <?php
-    class Conn {
-        public static function getConn() {
-            
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "liquorlibrary";
-            
-            if (isset($_ENV['DATABASE_CONNECTION_URL'])) {
-                $servername = $_ENV['DATABASE_CONNECTION_URL'];
-            }
-            if (isset($_ENV['DATABASE_NAME'])) {
-                $dbname = $_ENV['DATABASE_NAME'];
-            }
-            if (isset($_ENV['DATABASE_USERNAME'])) {
-                $dbname = $_ENV['DATABASE_USERNAME'];
-            }
-            if (isset($_ENV['DATABASE_PASSWORD'])) {
-                $dbname = $_ENV['DATABASE_PASSWORD'];
-            }
-            // Create connection
-            $conn = mysqli_connect($servername, $username, $password, $dbname);
-            
-            return $conn;
+class Conn
+{
+    public static function getConn()
+    {
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "liquorlibrary";
+
+        if (isset($_ENV['DATABASE_CONNECTION_URL'])) {
+            $servername = $_ENV['DATABASE_CONNECTION_URL'];
         }
+        if (isset($_ENV['DATABASE_NAME'])) {
+            $dbname = $_ENV['DATABASE_NAME'];
+        }
+        if (isset($_ENV['DATABASE_USERNAME'])) {
+            $username = $_ENV['DATABASE_USERNAME'];
+        }
+        if (isset($_ENV['DATABASE_PASSWORD'])) {
+            $password = $_ENV['DATABASE_PASSWORD'];
+        }
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        return $conn;
     }
+}
