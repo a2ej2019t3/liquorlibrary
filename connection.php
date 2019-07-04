6f61242043c0af570c519c156399988cf9efbd8b
JunboJ <junboz598@gmail.com>
2019-07-04 14:55:25 +1200

    connection

--- connection.php
+++ connection.php
@@ -1,7 +1,12 @@
 <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $dbname = "liquorlibrary";
+
+        $servername = "liquorlibrary.cve0pualyi6x.ap-southeast-2.rds.amazonaws.com";
+        $username = "llAdmin";
+        $password = "ll1234567!";
         $dbname = "liquorlibrary";
 
         // Create connection
--- database/DBconn.php
+++ database/DBconn.php
@@ -1,9 +1,14 @@
 <?php
     class Conn {
         public static function getConn() {
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
+            // $servername = "localhost";
+            // $username = "root";
+            // $password = "";
+            // $dbname = "liquorlibrary";
+
+            $servername = "liquorlibrary.cve0pualyi6x.ap-southeast-2.rds.amazonaws.com";
+            $username = "llAdmin";
+            $password = "ll1234567!";
             $dbname = "liquorlibrary";
     
             // Create connection
