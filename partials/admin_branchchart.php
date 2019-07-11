88646e84d6c5272e650fff7170cf0356598cdab8
ham8821 <ham38538821@gmail.com>
2019-07-12 09:23:29 +1200

    chart_modified

--- partials/admin_branchchart.php
+++ partials/admin_branchchart.php
@@ -16,18 +16,19 @@ if($branch_res){
                 if($branch_staff_res){
                     $branch_staff_arr=mysqli_fetch_all($branch_staff_res,MYSQLI_ASSOC);
                     for($b; $b< count($branch_staff_arr); $b++){
-                        $userID=$branch_staff_arr[$b]['userID'];
+                        $usersID=$branch_staff_arr[$b]['userID'];
                         $wareID=$branch_staff_arr[$b]['whID'];
-                            $getstafforder_sql="SELECT * from orders WHERE buyerID='$userID' AND `paymentmethod`='null' AND `deliverymethod`='null'";
+                            $getstafforder_sql="SELECT * from orders WHERE buyerID='$usersID' AND `paymentmethod`='null' AND `deliverymethod`='null'";
                             $getstafforder_res=mysqli_query($connection,$getstafforder_sql);
                             if($getstafforder_res){
                                 $getstafforder_arr= mysqli_fetch_all($getstafforder_res,MYSQLI_ASSOC);
-                                if(!empty($branchbackorder)){
-                                    $branchbackorder[$wareID]=array_merge( $branchbackorder[$wareID],$getstafforder_arr[$b]['orderID']);
-                                }
-                                else{
-                                    $branchbackorder[$wareID]=$getstafforder_arr[$b]['orderID'];
-                                }
+                                var_dump($getstafforder_arr);
+                                // if(!empty($branchbackorder)){
+                                //     $branchbackorder[$wareID]=array_merge( $branchbackorder[$wareID],$getstafforder_arr[$b]['orderID']);
+                                // }
+                                // else{
+                                //     $branchbackorder[$wareID]=$getstafforder_arr[$b]['orderID'];
+                                // }
                                 // var_dump($branchbackorder);
                             }
                     }
